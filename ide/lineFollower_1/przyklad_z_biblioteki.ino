ac1a31b59c9411763558343e19ae6120f67bb6ed
p-szymczak <ps307109@student.polsl.pl>
2024-12-16 16:21:11 +0100

    Create przyklad_z_biblioteki.ino

---
+++ ide/lineFollower_1/przyklad_z_biblioteki.ino
@@ -0,0 +1,18 @@
+void loop()
+{
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 5000 (for a white line, use readLineWhite() instead)
+  uint16_t position = qtr.readLineBlack(sensorValues);
+
+  // print the sensor values as numbers from 0 to 1000, where 0 means maximum
+  // reflectance and 1000 means minimum reflectance, followed by the line
+  // position
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  Serial.println(position);
+
+  delay(250);
+}
